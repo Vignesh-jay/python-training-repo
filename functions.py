782226b07c94c06907247658b5041555598a407e
Vignesh-jay <Vigneshviki428@gmail.com>
2022-04-23 12:48:42 +0530

    Update functions.py
    added Function with return values

--- functions.py
+++ functions.py
@@ -1,5 +1,16 @@
+#simple function
 def prin():
-    print("\nhello world\n")
+    print("\nhello world \n")
     print("good day\n")
 
 prin()
+
+#Calling funtion with returns
+
+def fun():
+    a=int(input("Enter a Value : "))
+    b=int(input("enter b Value : "))
+    c=a*b
+    return c
+
+print("the a * b value is ",fun())
