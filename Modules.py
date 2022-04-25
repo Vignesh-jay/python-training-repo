6a53be68294611e097b68a56f7a89a2e5d314011
Vignesh-jay <Vigneshviki428@gmail.com>
2022-04-25 16:56:23 +0530

    Modules

---
+++ Modules.py
@@ -0,0 +1,29 @@
+
+#random module
+
+# Dice rolling Program
+
+from random import randrange
+x=0
+for num in range(0,2):
+    value=randrange(1,6)
+    if value==1:
+        print("One : *")
+    elif value==2:
+        print("Two : **")
+    elif value==3:
+        print("Three : ***")
+    elif value==4:
+        print("Four : ****")
+    elif value==5:
+        print("Five : *****")
+    elif value==6:
+        print("Six : ******")
+    else:
+        print("Error...!")
+    x += value
+print("your value is : ",x)
+
+import math
+
+print(math.factorial(6))
